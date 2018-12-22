874a30bc6b101d9f464733091d141e4bb8a08859
GaganGupta <gagan_gupta111@yahoo.com>
2018-12-22 18:59:54 +0530

    only one field works for searching

--- src/main/java/com/loganalyzer/elasticsearch/receiver/LogEventsGenerator.java
+++ src/main/java/com/loganalyzer/elasticsearch/receiver/LogEventsGenerator.java
@@ -746,4 +746,3 @@ public class LogEventsGenerator extends LogFilePatternReceiver {
         System.out.println("=======================================================================================");
     }
 }
-
